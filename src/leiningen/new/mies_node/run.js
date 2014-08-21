9db70b5d0ff8682a4d37e280e02181d70122cfd8
David Nolen <dnolen@cognitect.com>
2014-08-21 11:39:41 -0400

    try to load source map support

--- src/leiningen/new/mies_node/run.js
+++ src/leiningen/new/mies_node/run.js
@@ -1,3 +1,7 @@
+try {
+    require("source-map-support").install();
+} catch(err) {
+}
 require("./out/goog/bootstrap/nodejs.js");
 require("./out/{{sanitized}}.js");
 goog.require("{{sanitized}}.core");
