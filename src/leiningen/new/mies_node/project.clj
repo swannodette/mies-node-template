b3b8cf48523b87ec7eca7b6f9d8be43ec3218125
Peter Schwarz <peter.schwarz@bicycle.io>
2014-08-26 10:52:16 -0500

    Add a basic unit test

--- src/leiningen/new/mies_node/project.clj
+++ src/leiningen/new/mies_node/project.clj
@@ -5,7 +5,10 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2311"]]
 
-  :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
+  :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]
+            [com.cemerick/clojurescript.test "0.3.1"]]
+
+  :hooks [leiningen.cljsbuild]
 
   :source-paths ["src"]
 
@@ -17,4 +20,14 @@
                 :output-dir "out"
                 :target :nodejs
                 :optimizations :none
-                :source-map true}}]})
+                :source-map true}}
+             {:id "test"
+              :source-paths ["src" "test"]
+              :compiler {
+                :output-to "out/{{sanitized}}-testable.js"
+                :output-dir "out/test"
+                :target :nodejs
+                :optimizations :simple
+                :hashbang false}}]
+              :test-commands {"unit-tests" ["node" :node-runner
+                                            "out/{{sanitized}}-testable.js"]}})
