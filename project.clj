82d4af814bde9b67e385c198085de66ff37cf5f3
David Nolen <david.nolen@gmail.com>
2015-01-03 21:33:00 -0500

    bump deps

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mies-node/lein-template "0.1.2"
+(defproject mies-node/lein-template "0.1.3"
   :description "A minimal ClojureScript Node.js project template"
 
   :url "http://github.com/swannodette/mies-node"
--- src/leiningen/new/mies_node/project.clj
+++ src/leiningen/new/mies_node/project.clj
@@ -3,11 +3,11 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2505"]]
+                 [org.clojure/clojurescript "0.0-2657"]]
 
   :node-dependencies [[source-map-support "0.2.8"]]
 
-  :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]
+  :plugins [[lein-cljsbuild "1.0.4"]
             [lein-npm "0.4.0"]]
 
   :source-paths ["src"]
