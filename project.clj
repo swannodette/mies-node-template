7dc3b13546283c6adde652a0549fd29dcb96adcd
David Nolen <david.nolen@gmail.com>
2015-01-24 14:26:34 -0500

    bump deps

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mies-node/lein-template "0.1.3"
+(defproject mies-node/lein-template "0.2.0"
   :description "A minimal ClojureScript Node.js project template"
 
   :url "http://github.com/swannodette/mies-node"
