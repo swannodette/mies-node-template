7bda9bdfef7b30f03cd05ba0c22fcef845ec2356
Jon Roes <jroes@jroes.net>
2015-03-19 04:56:55 -0400

    Bump clojurescript dep for node 0.12 support.

--- src/leiningen/new/mies_node/project.clj
+++ src/leiningen/new/mies_node/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2755"]]
+                 [org.clojure/clojurescript "0.0-3126"]]
 
   :node-dependencies [[source-map-support "0.2.8"]]
 
