91366e0ec7a6a762da7e265a351e8a49a5abf66f
Nandor Kracser <bonifaido@gmail.com>
2015-04-07 17:28:28 +0200

    Fixed GitHub project URLs

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject mies-node/lein-template "0.2.2"
   :description "A minimal ClojureScript Node.js project template"
 
-  :url "http://github.com/swannodette/mies-node"
+  :url "http://github.com/swannodette/mies-node-template"
 
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :scm {:name "git"
-        :url "https://github.com/swannodette/mies-node"}
+        :url "https://github.com/swannodette/mies-node-template"}
 
   :eval-in-leiningen true)
